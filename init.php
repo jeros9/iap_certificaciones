a3a7eb5795635b939acbe1ba87f1819a25f8766a
Daniel Lopez <dlopez@trazzos.com>
2020-07-18 19:18:31 -0500

    mods

--- init.php
+++ init.php
@@ -1,6 +1,6 @@
 <?php 
 ini_set("display_errors", 1);
-	error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_WARNING);
+	error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
 	
 	if(function_exists('xdebug_disable'))
 		xdebug_disable();
