9605a6c206ca6b314ac57980f978fc59f5241331
Carlos Zenteno <carloszh04@gmail.com>
2022-01-28 12:13:13 -0600

    Display Error 0

--- init.php
+++ init.php
@@ -1,6 +1,7 @@
 <?php 
-ini_set("display_errors", 1);
-	error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
+error_reporting(0);
+/* ini_set("display_errors", 1);
+error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED); */
 	
 	if(function_exists('xdebug_disable'))
 		xdebug_disable();
