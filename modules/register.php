b4bc95db1baba08cd4e4ba46337f0198cf6e3d88
William Ramírez <wramirez@iapchiapas.edu.mx>
2025-01-31 15:26:19 -0600

    Url de registro solo para usuarios autenticados

--- modules/register.php
+++ modules/register.php
@@ -1,22 +1,20 @@
 <?php
-	
-	/* For Session Control - Don't remove this */
-	//$user->allow_access(4);	
-	/* End Session Control */
+
+/* For Session Control - Don't remove this */
+$user->allow_access();
+/* End Session Control */
 $student->setCountry(1);
 $estados = $student->EnumerateEstados();
-$lstSolicitante=$student->EnumerateSolicitantes();	
+$lstSolicitante = $student->EnumerateSolicitantes();
 
-$smarty->assign('paises',$paises);
-$smarty->assign('estados',$estados);
+$smarty->assign('paises', $paises);
+$smarty->assign('estados', $estados);
 
 $prof = $profesion->Enumerate();
 $prof = $util->EncodeResult($prof);
-$smarty->assign('prof',$prof);
+$smarty->assign('prof', $prof);
 $activeCourses = $course->EnumerateActive();
-$personalCapacitado = $personal->EnumerateSubject($activeCourses[0]['subjectId']); 
-$smarty->assign("lstSolicitante", $lstSolicitante);	
-$smarty->assign("activeCourses", $activeCourses);	
-$smarty->assign("personalCapacitado",$personalCapacitado);
-	
-?>
+$personalCapacitado = $personal->EnumerateSubject($activeCourses[0]['subjectId']);
+$smarty->assign("lstSolicitante", $lstSolicitante);
+$smarty->assign("activeCourses", $activeCourses);
+$smarty->assign("personalCapacitado", $personalCapacitado);
